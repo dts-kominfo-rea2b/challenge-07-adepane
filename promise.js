644782690ed58c55d98f1bcdc690d10a178d2b41
adepane <adesbpane@gmail.com>
2022-06-12 01:05:34 +0700

    Challenge 07 Solution

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (effect) => {
+  try {
+    const ixxAudiance = await promiseTheaterIXX();
+    const vcgAudiance = await promiseTheaterVGC();
+    const theaterEff = await ixxAudiance.concat(vcgAudiance)
+    return theaterEff.filter(x => x.hasil == effect).length;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
   promiseOutput,
